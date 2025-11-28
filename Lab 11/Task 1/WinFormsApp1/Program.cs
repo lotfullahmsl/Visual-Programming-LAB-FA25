c59d9e53c060cc4d943b95f186f798693791f5e1
Lotfullah Muslimwal <149297929+lotfullahmsl@users.noreply.github.com>
2025-11-28 19:58:06 +0500

    Add files via upload

---
+++ Lab 11/Task 1/WinFormsApp1/Program.cs
@@ -0,0 +1,36 @@
+/*namespace WinFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}*/
+
+using System;
+using System.Windows.Forms;
+using Lab11;
+
+namespace Lab11
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // Run our custom Form1
+            Application.Run(new Form1());
+        }
+    }
+}
